f4cd9650221020a825afd798e0476b7f00ae17dd
Ben <bcootner@gmail.com>
2017-01-30 20:52:28 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use stric';
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var port = process.env.PORT || 8080;
+
+app.get('/',function(req,res){
+	console.log("homepage hit")
+	res.sendFile(path.join(__dirname, /public/test.html));
+});
+
+app.get('/hidden',function(req,res){
+	console.log("hidden found")
+});
