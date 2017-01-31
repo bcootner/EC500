28b4941ae12699dbbd659e01db5a7f54b4b918a0
Ben <bcootner@gmail.com>
2017-01-30 21:16:14 -0500

    added path

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 8080;
 
+//Tells express to look in the public folder for the assets 
 app.use(express.static(__dirname + '/public'));
 
+//Main page
 app.get('/',function(req,res){
 	console.log("homepage hit");
 	res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
+// /hidden directory 
 app.get('/hidden',function(req,res){
 	console.log("hidden found");
 });
 
-app.listen(port, function() {
-	console.log("scarletfish is listening on port" + port);
-})
+// app.listen(port, function() {
+// 	console.log("scarletfish is listening on port" + port);
+// })
