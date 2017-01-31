fa7596b75de7ef02b66fb899eba255e8644d771a
Ben <bcootner@gmail.com>
2017-01-30 21:01:50 -0500

    add quotes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 
 app.get('/',function(req,res){
 	console.log("homepage hit")
-	res.sendFile(path.join(__dirname, /public/test.html));
+	res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
 app.get('/hidden',function(req,res){
