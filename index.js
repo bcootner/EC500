a43f0dc236fabcf56b4131ca7974143a811514f4
Ben <bcootner@gmail.com>
2017-02-21 18:35:01 -0500

    added db intgeration

--- index.js
+++ index.js
@@ -2,14 +2,31 @@
 
 var express = require('express');
 var path = require('path');
+var pgp = require('pg-promise')();
 var app = express();
 var port = process.env.PORT || 8080;
+
+var db = pgp('postgres://djlciyuhmnrckz:863b9dcbf6f076e322b38b2e2e62812b5ca32f301205941fb0c2b8725cf1cf9d@ec2-54-235-72-121.compute-1.amazonaws.com:5432/ddv40oul581abb');
 
 //Tells express to look in the public folder for the assets 
 app.use(express.static(__dirname + '/public'));
 
 //Use pug for templated pages
 app.set('view engine', 'pug')
+
+//Search database for matching params 
+app.get('/login/:email/:password', function(req,res){
+	console.log("login page!")
+	console.log(email);
+	console.log(password);
+	db.one('SELECT * FROM users WHERE email_address=$1 AND password=$2', req.params.email, req.params.password)
+	.then(function(data){
+		console.log("DATA:", data.value);
+	})
+	.catch(function(error){
+		console.log("error", error);
+	})
+});
 
 
 //Main page
