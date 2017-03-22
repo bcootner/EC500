e26c4ec2cc9b731049ff765c5a8cda78f6b41cef
Samuel Beaulieu <sambeau@bu.edu>
2017-03-22 19:06:56 -0400

    change profile to pug

---
+++ public/profileScripts.js
@@ -0,0 +1,5 @@
+
+function updateFeatures() {
+    document.getElementById("features").innerHTML = "You can post!";
+    document.getElementById("name").innerHTML = "<img src=\"profile-42914_960_720.jpg\" style=\"width: 200px; padding-right: 10px\" />" + "Sam Beaulieu";
+}
