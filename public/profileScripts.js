1da9ac5b1af18baf1c1096f43af5d55669bc969e
Samuel Beaulieu <sambeau@bu.edu>
2017-04-03 19:16:48 -0400

    update image string

--- public/profileScripts.js
+++ public/profileScripts.js
@@ -2,7 +2,7 @@
 function updateFeatures() {
 	var first = data.first_name;
 	var last = data.last_name;
-	var img = "img src=\"profile-42914_960_720.jpg\" style=\"width: 200px; padding-right: 10px\" />";
+	var img = "img(src=\"profile-42914_960_720.jpg\" style=\"width: 200px; padding-right: 10px\"";
 
 	var header = img + first + " " + last + "WOOOOO";
 	document.getElementById("name")
