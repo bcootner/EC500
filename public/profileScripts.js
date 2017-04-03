f329f1adec319558e79e2b1c977f0fc54cb78249
Samuel Beaulieu <sambeau@bu.edu>
2017-04-03 19:20:20 -0400

    update image string

--- public/profileScripts.js
+++ public/profileScripts.js
@@ -2,9 +2,9 @@
 function updateFeatures() {
 	var first = data.first_name;
 	var last = data.last_name;
-	var img = "img(src=\"profile-42914_960_720.jpg\" style=\"width: 200px; padding-right: 10px\"";
+	//var img = "img(src=\"profile-42914_960_720.jpg\" style=\"width: 200px; padding-right: 10px\"";
 
-	var header = img + first + " " + last + "WOOOOO";
+	var header = /*img + */first + " " + last + "WOOOOO";
 	document.getElementById("name")
     document.getElementById("features").innerHTML = "You can post!";
     document.getElementById("name").innerHTML = header;
